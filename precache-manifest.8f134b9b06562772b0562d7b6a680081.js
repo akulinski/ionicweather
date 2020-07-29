4fd15152c911713625e9939c77c12d17fa3e97db
Albert <you@example.com>
2020-07-29 17:36:46 +0200

    Updates

---
+++ precache-manifest.8f134b9b06562772b0562d7b6a680081.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4087f455f221bf2783ef0ff63abee86",
+    "url": "/ionicweather/index.html"
+  },
+  {
+    "revision": "fed2d714ef7a373cfed2",
+    "url": "/ionicweather/static/css/4.8921c4c4.chunk.css"
+  },
+  {
+    "revision": "d4df0e47a6275ea0a647",
+    "url": "/ionicweather/static/css/main.77201012.chunk.css"
+  },
+  {
+    "revision": "d6549c7202bed9d63f89",
+    "url": "/ionicweather/static/js/0.b9788e5d.chunk.js"
+  },
+  {
+    "revision": "59e9f58d5c0f341327cf",
+    "url": "/ionicweather/static/js/10.85f1d750.chunk.js"
+  },
+  {
+    "revision": "ecd45535dbc4d5765014",
+    "url": "/ionicweather/static/js/11.130867b4.chunk.js"
+  },
+  {
+    "revision": "ce546ab49c695ab13d50",
+    "url": "/ionicweather/static/js/12.611f116b.chunk.js"
+  },
+  {
+    "revision": "1acd829cd72edacb53ed",
+    "url": "/ionicweather/static/js/13.093b364b.chunk.js"
+  },
+  {
+    "revision": "ea9e4a5c8725963f21a7",
+    "url": "/ionicweather/static/js/14.f52fa192.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/ionicweather/static/js/14.f52fa192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04fe78d8d54ab0ca0c4a",
+    "url": "/ionicweather/static/js/15.d2e7da5d.chunk.js"
+  },
+  {
+    "revision": "827731af76818f421958",
+    "url": "/ionicweather/static/js/16.53cda533.chunk.js"
+  },
+  {
+    "revision": "9b7d3d20c27a4d12e023",
+    "url": "/ionicweather/static/js/17.aafae722.chunk.js"
+  },
+  {
+    "revision": "a4f59cbc5b815daa3c95",
+    "url": "/ionicweather/static/js/18.89c724f0.chunk.js"
+  },
+  {
+    "revision": "2f9ad5d41b434a49ff43",
+    "url": "/ionicweather/static/js/19.b5cda61a.chunk.js"
+  },
+  {
+    "revision": "b5178dd389631ca33f73",
+    "url": "/ionicweather/static/js/20.83fec4ee.chunk.js"
+  },
+  {
+    "revision": "4c4582745f93a7d5df64",
+    "url": "/ionicweather/static/js/21.d17c933a.chunk.js"
+  },
+  {
+    "revision": "cd9b5a67996aac50d159",
+    "url": "/ionicweather/static/js/22.a8774884.chunk.js"
+  },
+  {
+    "revision": "a8b156d1ce4623711818",
+    "url": "/ionicweather/static/js/23.e50c1c0e.chunk.js"
+  },
+  {
+    "revision": "c9812f90e774b25b7aca",
+    "url": "/ionicweather/static/js/24.b6dc5134.chunk.js"
+  },
+  {
+    "revision": "040c1c8ff38545b53c50",
+    "url": "/ionicweather/static/js/25.c691e86f.chunk.js"
+  },
+  {
+    "revision": "b4cda610b61b5a30c8fd",
+    "url": "/ionicweather/static/js/26.5dd435ac.chunk.js"
+  },
+  {
+    "revision": "6df397797b7a7cb445da",
+    "url": "/ionicweather/static/js/27.9de77f1c.chunk.js"
+  },
+  {
+    "revision": "5cceaf2e7eb1922eb4a1",
+    "url": "/ionicweather/static/js/28.7557d034.chunk.js"
+  },
+  {
+    "revision": "e6afc8271acacf36e535",
+    "url": "/ionicweather/static/js/29.dade82c7.chunk.js"
+  },
+  {
+    "revision": "bc6551d8eae23ebe59bf",
+    "url": "/ionicweather/static/js/30.caf15a98.chunk.js"
+  },
+  {
+    "revision": "b080485643c3aaf3bcac",
+    "url": "/ionicweather/static/js/31.d4ec348e.chunk.js"
+  },
+  {
+    "revision": "5c8944a1366f681600a4",
+    "url": "/ionicweather/static/js/32.e8ddf1ec.chunk.js"
+  },
+  {
+    "revision": "6a4cd887ef8d783456eb",
+    "url": "/ionicweather/static/js/33.2199e214.chunk.js"
+  },
+  {
+    "revision": "45ab0e6222394d9fc32b",
+    "url": "/ionicweather/static/js/34.06466b89.chunk.js"
+  },
+  {
+    "revision": "f6f8ef51ab949b3b23cd",
+    "url": "/ionicweather/static/js/35.54db92a6.chunk.js"
+  },
+  {
+    "revision": "cb6f6906fa1515afe8c0",
+    "url": "/ionicweather/static/js/36.c4f08034.chunk.js"
+  },
+  {
+    "revision": "77975a593706e7a105ce",
+    "url": "/ionicweather/static/js/37.6ed47945.chunk.js"
+  },
+  {
+    "revision": "c1dcac4a9ac79c1bf810",
+    "url": "/ionicweather/static/js/38.13517ae1.chunk.js"
+  },
+  {
+    "revision": "7e86bb58c05550f283dc",
+    "url": "/ionicweather/static/js/39.ef4379f2.chunk.js"
+  },
+  {
+    "revision": "fed2d714ef7a373cfed2",
+    "url": "/ionicweather/static/js/4.629c741a.chunk.js"
+  },
+  {
+    "revision": "2903f65e346887768e2d109a63fdd7a4",
+    "url": "/ionicweather/static/js/4.629c741a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "807baec4038d2073d411",
+    "url": "/ionicweather/static/js/40.48c9494e.chunk.js"
+  },
+  {
+    "revision": "65ed6ff536d9a512da6c",
+    "url": "/ionicweather/static/js/41.903014bd.chunk.js"
+  },
+  {
+    "revision": "6a1271a7d8027b46d91c",
+    "url": "/ionicweather/static/js/42.711f76fb.chunk.js"
+  },
+  {
+    "revision": "f224e8ed4734f04fce6b",
+    "url": "/ionicweather/static/js/43.bd846ead.chunk.js"
+  },
+  {
+    "revision": "35b431ffb1429312ae66",
+    "url": "/ionicweather/static/js/44.153735e7.chunk.js"
+  },
+  {
+    "revision": "52cd38570309916dffab",
+    "url": "/ionicweather/static/js/45.f98a6d78.chunk.js"
+  },
+  {
+    "revision": "1e3b183476adba007158",
+    "url": "/ionicweather/static/js/46.48e8733c.chunk.js"
+  },
+  {
+    "revision": "04bb2a0ae594b6bf2e97",
+    "url": "/ionicweather/static/js/47.16d9ea8f.chunk.js"
+  },
+  {
+    "revision": "55fd262ff3c14a480d8b",
+    "url": "/ionicweather/static/js/48.26c49f08.chunk.js"
+  },
+  {
+    "revision": "6226b80c04d69c480cd2",
+    "url": "/ionicweather/static/js/49.4f07e08a.chunk.js"
+  },
+  {
+    "revision": "27b8eaa47a7dfb50523c",
+    "url": "/ionicweather/static/js/5.bad0e8ec.chunk.js"
+  },
+  {
+    "revision": "6d817171030f37fe5722",
+    "url": "/ionicweather/static/js/50.fe6b27a4.chunk.js"
+  },
+  {
+    "revision": "94e30957217a1ca038d0",
+    "url": "/ionicweather/static/js/51.f883a108.chunk.js"
+  },
+  {
+    "revision": "8ac1043e68f6efa39edb",
+    "url": "/ionicweather/static/js/52.1938b1a1.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/ionicweather/static/js/52.1938b1a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858f40e2e4954bde85bd",
+    "url": "/ionicweather/static/js/53.4c708a00.chunk.js"
+  },
+  {
+    "revision": "5a449adacfd1bc99bc29",
+    "url": "/ionicweather/static/js/54.211381c5.chunk.js"
+  },
+  {
+    "revision": "e2fe9322de33a60d5b0d",
+    "url": "/ionicweather/static/js/55.323dfdfe.chunk.js"
+  },
+  {
+    "revision": "d154b9c89cadfd2ade15",
+    "url": "/ionicweather/static/js/56.ac134226.chunk.js"
+  },
+  {
+    "revision": "8b4197b9cfe62ce5b061",
+    "url": "/ionicweather/static/js/57.bca33028.chunk.js"
+  },
+  {
+    "revision": "da687e00d17d3c172dcd",
+    "url": "/ionicweather/static/js/58.1d07ca2d.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/ionicweather/static/js/58.1d07ca2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e27ad9b5dcf8fc13fcd",
+    "url": "/ionicweather/static/js/6.a8d949ef.chunk.js"
+  },
+  {
+    "revision": "3e29392b6a98295d3ddc",
+    "url": "/ionicweather/static/js/7.e13f3449.chunk.js"
+  },
+  {
+    "revision": "0b7e1555298397da3d05",
+    "url": "/ionicweather/static/js/8.ca3e7111.chunk.js"
+  },
+  {
+    "revision": "054694ce6de1df9bec70",
+    "url": "/ionicweather/static/js/9.6f74ee74.chunk.js"
+  },
+  {
+    "revision": "d4df0e47a6275ea0a647",
+    "url": "/ionicweather/static/js/main.ec198663.chunk.js"
+  },
+  {
+    "revision": "9cc12dd54810385804d2",
+    "url": "/ionicweather/static/js/polyfills-css-shim.2b95784a.chunk.js"
+  },
+  {
+    "revision": "d73e927b24cf1351e29c",
+    "url": "/ionicweather/static/js/runtime-main.1c6074e6.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ionicweather/precache-manifest.8f134b9b06562772b0562d7b6a680081.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ionicweather/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
